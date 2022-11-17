7163efcb3919ae037a8efa32eb6ec121882b725f
SQuan <quann285@gmail.com>
2022-11-17 09:39:48 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { SearchComponent } from './components/search/search.component';
+import { NewComponent } from './components/new/new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavBarComponent,
+    FooterComponent,
+    LoginComponent,
+    CartComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    SearchComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
